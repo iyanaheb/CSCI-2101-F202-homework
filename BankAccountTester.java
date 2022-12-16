1303ff5f3e3f160e494871947238573cb627912b
iyanaheb <barnesi@go.stockton.edu>
2022-12-16 10:36:33 -0500

    banktester

---
+++ BankAccountTester.java
@@ -0,0 +1,23 @@
+public class BankAccountTester {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Bank Account Tester");
+        // Example Bank Account used for Transfer Methods
+
+        BankAccount gregChecking = new BankAccount(2000);
+        BankAccount newChecking = new BankAccount(4000);
+        
+        // Tranfer
+        gregChecking.withdraw(100);
+        System.out.println(gregChecking.getBalance());
+        newChecking.deposit(100);
+        System.out.println(newChecking.getBalance());
+   
+
+    
+ 
+
+        BankAccount.interact(gregChecking);
+        
+
+   }
+} 
