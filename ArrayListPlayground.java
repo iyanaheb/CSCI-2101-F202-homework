9dd812c7fc6b964a0f1b01ced879ee791045efc2
iyanaheb <barnesi@go.stockton.edu>
2022-12-16 23:52:38 -0500

    finished

--- ArrayListPlayground.java
+++ ArrayListPlayground.java
@@ -15,15 +15,30 @@ public class ArrayListPlayground {
         // d) set the value of the 2nd element to 6
         // d) Set the value of the 9th element of the array nums to the sum of the 13th
         // and 2nd elements of the array nums.
-        /*ArrayList<String> num = new ArrayList<>(7);
-        daysOfWeek.add("Monday");
-        daysOfWeek.add("Tuesday");
-        daysOfWeek.add("Wednesday");
-        daysOfWeek.add("Thursday");
-        daysOfWeek.add("Friday");
-        daysOfWeek.add("Saturday");
-        daysOfWeek.add("Sunday");
-*/
+        int[] nums;
+        // allocate memory for 10 integers
+        nums = new int[15];
+
+        System.out.println(nums[0]);
+        // Set value at index 0 to 100
+        ;
+        nums[0] = 100;
+        nums[1] = 6;
+        nums[2]= 300;
+        nums[3]= 400;
+        nums[4]= 99;
+        nums[5]= 600;
+        nums[6]= 700;
+        nums[7]= 800;
+        nums[8]= nums[2]+nums[12];
+        nums[9]= 1000;
+        nums[10] = 200;
+        nums[11]= 300;
+        nums[12]= 15;
+        nums[13]= 500;
+        nums[5]= 600;
+        System.out.println("Element at index - 10: " + nums[9]);
+        System.out.println("Element at index - 8: " + nums[8]);
         // Question 2
         // a) create an arrayList of Strings that contain each day of the week.(starting
         // on monday)
